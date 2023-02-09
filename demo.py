b4c48881597381bb880a071b8408152c8d9b26b8
jsl339 <johnleland@Johns-MacBook-Air.local>
2023-02-09 11:10:56 -0700

    add everything

---
+++ demo.py
@@ -0,0 +1,128 @@
+# Chang Wei Tan, Christoph Bergmeir, Francois Petitjean, Geoff Webb
+#
+# @article{
+#   Tan2020TSER,
+#   title={Time Series Extrinsic Regression},
+#   author={Tan, Chang Wei and Bergmeir, Christoph and Petitjean, Francois and Webb, Geoffrey I},
+#   journal={Data Mining and Knowledge Discovery},
+#   pages={1--29},
+#   year={2021},
+#   publisher={Springer},
+#   doi={https://doi.org/10.1007/s10618-021-00745-9}
+# }
+import argparse
+
+import numpy as np
+
+from utils.data_loader import load_from_tsfile_to_dataframe
+from utils.regressor_tools import process_data, fit_regressor, calculate_regression_metrics
+from utils.tools import create_directory
+from utils.transformer_tools import fit_transformer
+
+
+module = "RegressionExperiment"
+
+# transformer parameters
+transformer_name = "none"  # see transformer_tools.transformers
+flatten = False  # if flatten, do not transform per dimension
+n_components = 10  # number of principal components
+n_basis = 10  # number of basis functions
+bspline_order = 4  # bspline order
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--data", required=False, default="/Users/johnleland/Desktop/TS-Extrinsic-Regression/Monash_UEA_UCR_Regression_Archive/")
+parser.add_argument("-p", "--problem", required=False, default="AppliancesEnergy")  # see data_loader.regression_datasets
+parser.add_argument("-c", "--regressor", required=False, default="fcnattention")  # see regressor_tools.all_models
+parser.add_argument("-i", "--iter", required=False, default=10)
+parser.add_argument("-n", "--norm", required=False, default="none")  # none, standard, minmax
+
+arguments = parser.parse_args()
+
+# start the program
+if __name__ == '__main__':
+    data_path = arguments.data
+    problem = arguments.problem  # see data_loader.regression_datasets
+    regressor_name = arguments.regressor  # see regressor_tools.all_models
+    itr = arguments.iter
+    norm = arguments.norm  # none, standard, minmax
+
+    # create output directory
+    output_directory = "output/regression/"
+    if norm != "none":
+        output_directory = "output/regression_{}/".format(norm)
+    output_directory = output_directory + regressor_name + '/' + problem + '/itr_' + str(itr) + '/'
+    create_directory(output_directory)
+
+    print("=======================================================================")
+    print("[{}] Starting Holdout Experiments".format(module))
+    print("=======================================================================")
+    print("[{}] Data path: {}".format(module, data_path))
+    print("[{}] Output Dir: {}".format(module, output_directory))
+    print("[{}] Iteration: {}".format(module, itr))
+    print("[{}] Problem: {}".format(module, problem))
+    print("[{}] Regressor: {}".format(module, regressor_name))
+    print("[{}] Transformer: {}".format(module, transformer_name))
+    print("[{}] Normalisation: {}".format(module, norm))
+
+    # set data folder, train & test
+    data_folder = data_path + problem + "/"
+    train_file = data_folder + problem + "_TRAIN.ts"
+    test_file = data_folder + problem + "_TEST.ts"
+
+    # loading the data. X_train and X_test are dataframe of N x n_dim
+    print("[{}] Loading data".format(module))
+    X_train, y_train = load_from_tsfile_to_dataframe(train_file)
+    X_test, y_test = load_from_tsfile_to_dataframe(test_file)
+
+    print("[{}] X_train: {}".format(module, X_train.shape))
+    print("[{}] X_test: {}".format(module, X_test.shape))
+
+    # in case there are different lengths in the dataset, we need to consider that.
+    # assume that all the dimensions are the same length
+    print("[{}] Finding minimum length".format(module))
+    min_len = np.inf
+    for i in range(len(X_train)):
+        x = X_train.iloc[i, :]
+        all_len = [len(y) for y in x]
+        min_len = min(min(all_len), min_len)
+    for i in range(len(X_test)):
+        x = X_test.iloc[i, :]
+        all_len = [len(y) for y in x]
+        min_len = min(min(all_len), min_len)
+    print("[{}] Minimum length: {}".format(module, min_len))
+
+    # process the data into numpy array with (n_examples, n_timestep, n_dim)
+    print("[{}] Reshaping data".format(module))
+    x_train = process_data(X_train, normalise=norm, min_len=min_len)
+    x_test = process_data(X_test, normalise=norm, min_len=min_len)
+
+    # transform the data if needed
+    if transformer_name != "none":
+        if transformer_name == "pca":
+            kwargs = {"n_components": n_components}
+        elif transformer_name == "fpca":
+            kwargs = {"n_components": n_components}
+        elif transformer_name == "fpca_bspline":
+            kwargs = {"n_components": n_components,
+                      "n_basis": n_basis,
+                      "order": bspline_order,
+                      "smooth": "bspline"}
+        else:
+            kwargs = {}
+        x_train, transformer = fit_transformer(transformer_name, x_train, flatten=flatten, **kwargs)
+        x_test = transformer.transform(x_test)
+    print("[{}] X_train: {}".format(module, x_train.shape))
+    print("[{}] X_test: {}".format(module, x_test.shape))
+
+    # fit the regressor
+    regressor = fit_regressor(output_directory, regressor_name, x_train, y_train, x_test, y_test, itr=itr)
+
+    # start testing
+    y_pred = regressor.predict(x_test)
+    df_metrics = calculate_regression_metrics(y_test, y_pred)
+
+    print(df_metrics)
+
+    # save the outputs
+    df_metrics.to_csv(output_directory + 'regression_experiment.csv', index=False)
---
+++ models/deep_learning/mhattention.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+
+from models.deep_learning.deep_learning_models import DLRegressor
+
+
+class MHAttentionRegressor(DLRegressor):
+    """
+    This is a class implementing the Attention model for time series regression.
+    """
+
+    def __init__(
+            self,
+            output_directory,
+            input_shape,
+            verbose=False,
+            epochs=1000,
+            head_size = 2,
+            num_heads = 2,
+            ff_dim = 4,
+            dropout = 0,
+            batch_size=16,
+            loss="mean_squared_error",
+            metrics=None
+    ):
+        """
+        Initialise the FCN model
+
+        Inputs:
+            output_directory: path to store results/models
+            input_shape: input shape for the models
+            verbose: verbosity for the models
+            epochs: number of epochs to train the models
+            batch_size: batch size to train the models
+            loss: loss function for the models
+            metrics: metrics for the models
+        """
+
+        self.name = "mhattention"
+        self.head_size = head_size
+        self.num_heads = num_heads
+        self.ff_dim = ff_dim
+        self.dropout = dropout
+        super().__init__(
+            output_directory=output_directory,
+            input_shape=input_shape,
+            verbose=verbose,
+            epochs=epochs,
+            batch_size=batch_size,
+            loss=loss,
+            metrics=metrics
+        )
+    def transformer_encoder(self,inputs):
+        # Normalization and Attention
+        x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(inputs)
+        x = tf.keras.layers.MultiHeadAttention(
+            key_dim=self.head_size, num_heads=self.num_heads, dropout=self.dropout)(x, x)
+        return x
+
+    def build_model(self, input_shape):
+        """
+        Build the attention model
+
+        Inputs:
+            input_shape: input shape for the model
+        """
+
+        input_value = tf.keras.Input(input_shape)
+
+        conv1 = tf.keras.layers.Conv1D(filters=128,
+                                       kernel_size=8,
+                                       padding='same')(input_value)
+        conv1 = tf.keras.layers.BatchNormalization()(conv1)
+        conv1 = tf.keras.layers.Activation(activation='relu')(conv1)
+
+        attention1 = self.transformer_encoder(conv1)
+
+        conv2 = tf.keras.layers.Conv1D(filters=256,
+                                       kernel_size=5,
+                                       padding='same')(attention1)
+        conv2 = tf.keras.layers.BatchNormalization()(conv2)
+        conv2 = tf.keras.layers.Activation('relu')(conv2)
+
+        attention2 = self.transformer_encoder(conv2)
+
+        conv3 = tf.keras.layers.Conv1D(128,
+                                       kernel_size=3,
+                                       padding='same')(attention2)
+        conv3 = tf.keras.layers.BatchNormalization()(conv3)
+        conv3 = tf.keras.layers.Activation('relu')(conv3)
+
+        dense = tf.keras.layers.Dense(10, activation="relu")(conv3)
+        gap_layer = tf.keras.layers.GlobalAveragePooling1D()(dense)
+
+
+        output_layer = tf.keras.layers.Dense(1, activation='linear')(gap_layer)
+
+        model = tf.keras.models.Model(inputs=input_value, outputs=output_layer)
+
+        model.compile(loss=self.loss,
+                      optimizer=tf.keras.optimizers.Adam(),
+                      metrics=self.metrics)
+
+        return model
